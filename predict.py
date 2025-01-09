370bdc7e4ae120f7adcd1e32f74ad6d90f03f298
Tanguy Gellon <tanguy.gellon@gmail.com>
2025-01-09 16:59:47 +0100

    [PREDICT] predict program working

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
 import sys
+import json
+import os
 
 
 def main():
@@ -8,7 +10,19 @@ def main():
     if not sys.argv[1].isdigit():
         print("Error: You must enter a mileage (positive whole number).")
         sys.exit()
-    try:
+    if os.path.exists("train_data.json"):
+        with open("train_data.json", "r") as file:
+            train_data = json.load(file)
+            theta0 = train_data["theta0"]
+            theta1 = train_data["theta1"]
+    else:
+        theta0 = 0
+        theta1 = 0
+    mileage = int(sys.argv[1])
+    price = (theta0 * mileage) + theta1
+    if price < 0:
+        price = 0
+    print(f"The estimated price for a car with {mileage} km is {price:.0f} €.")
 
 
 
